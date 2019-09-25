7da039e4252fe34a645dd91a6540f688cf2e2575
kasariSaurabh <55755970+kasariSaurabh@users.noreply.github.com>
2019-09-25 13:35:55 +0530

    Add files via upload

---
+++ src/test/java/webdriver/Test1.java
@@ -0,0 +1,20 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void m()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\chrome77\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+	}
+
+}
